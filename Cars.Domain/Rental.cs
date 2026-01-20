39f3a2d5e47a1899672f0237130bb0cebe3b80e2
xketris <xketris@gmail.com>
2026-01-20 06:06:19 +0100

    Rental model

---
+++ Cars.Domain/Rental.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars.Domain
+{
+    public enum RentalStatus
+    {
+        Pending,
+        Confirmed,
+        Active,
+        Completed,
+        Cancelled
+    }
+
+    public class Rental
+    {
+        public Guid Id { get; set; }
+
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+
+        public Guid CarId { get; set; }
+        public Car Car { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public decimal TotalPrice { get; set; }
+        public RentalStatus Status { get; set; } = RentalStatus.Pending;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}

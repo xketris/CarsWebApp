0eb0120df9a1ec7688a62267a66bb5b2954e5036
xketris <xketris@gmail.com>
2026-01-20 08:28:04 +0100

    rentals controller

---
+++ Cars.API/Controllers/RentalsController.cs
@@ -0,0 +1,35 @@
+﻿using Cars.Application.Core;
+using Cars.Application.Rentals;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Cars.API.Controllers
+{
+    public class RentalsController : BaseApiController
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetMyRentals()
+        {
+            var username = User.FindFirstValue(ClaimTypes.Name) ?? User.FindFirstValue(ClaimTypes.Email);
+
+            return HandleResult(await Mediator.Send(new ListRentals.Query { Username = username }));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRental(CreateRental.Command command)
+        {
+            command.Username = User.FindFirstValue(ClaimTypes.Name) ?? User.FindFirstValue(ClaimTypes.Email);
+
+            return HandleResult(await Mediator.Send(command));
+        }
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+            if (result == null) return NotFound();
+            if (result.IsSuccess && result.Value != null) return Ok(result.Value);
+            if (result.IsSuccess && result.Value == null) return NotFound();
+            return BadRequest(result.Error);
+        }
+
+    }
+}
